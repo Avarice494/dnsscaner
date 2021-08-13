60bb1cfdfc075f9550ec635e85be9d7b9c30adcf
Administrator <1231>
2021-08-13 21:41:53 +0800

    默认更改列表

--- dnsscaner.py
+++ dnsscaner.py
@@ -8,56 +8,61 @@ from qqwry import QQwry
 import sys
 import getopt
 
+# import os
+# os.environ["http_proxy"] = "http://127.0.0.1:1080"
+# os.environ["https_proxy"] = "http://127.0.0.1:1080"
+#查询dns信息
 class dnsinfo:
     #
     def A(self,dname):
         try:
             A= resolver.query(dname,'A')
             for i in A.response.answer:
-                print(i)
+                for j in i.items:
+                    print("     A:"+str(j))
         except:
             print(dname+" NO A text!")
 
 
-    def N(self,dname):
+    def NS(self,dname):
         try:
             NS = resolver.query(dname,"NS")
             for i in NS.response.answer:
-                print(dname+":"+str(i))
+                print("       NS:"+str(i))
         except:
             print(dname + " NO NS text!")
 
-    def M(self,dname):
+    def MX(self,dname):
         try:
             MX =resolver.query(dname, 'MX')
             for i in MX:
-                print('MX preference =', i.preference, 'mail exchanger =', i.exchange)
+                print('     MX preference =', i.preference, 'mail exchanger =', i.exchange)
         except:
             print(dname+" NO MX text")
 
 
-    def T(self,dname):
+    def TXT(self,dname):
         try:
             TXT = resolver.query(dname,"TXT")
             for i in TXT.response.answer:
-                print(dname+":"+str(i))
+                print("     TXT:"+str(i))
         except:
             print(dname + " NO TXT text")
 
-    def C(self,dname):
+    def Cname(self,dname):
         try:
             Cname = resolver.query(dname,'CNAME')
             for i in  Cname:
-                print(dname+":"+str(i))
+                print("     Cname:"+str(i))
         except:
             print(dname+" NO CNAME text")
 
 
-    def S(self,dname):
+    def SOA(self,dname):
         try:
             SOA = resolver.query(dname,"SOA")
             for i in SOA:
-                print(" "+"SOA:"+str(i))
+                print("     SOA:"+str(i))
         except:
             print(" NO SOA text")
 
@@ -69,11 +74,12 @@ class dnsinfo:
     def PTR(self,dname):
         pass
 
+#查询whois信息
 class whoisinfo:
-    def update(self):
+    def Update(self):
         ret = updateQQwry('qqwryupdate.dat')
 
-    def whois(self,dname):
+    def Whois(self,dname):
         wry = QQwry()
         wry.load_file("qqwryupdate.dat")
         try:
@@ -82,80 +88,100 @@ class whoisinfo:
                 for j in i.items:
                     info = wry.lookup(str(j))
                     res = {"city": info[0], "isp": info[1]}
-                    print(res)
+                    print("     WHOIS:"+str(res))
         except:
             print(dname+" NO A text!")
 
 class out():
-    def str(self,domain,type):
-        print(domain+":")
-        eval("cl."+type[1:].upper()+"('"+domain+"')")
+    def str(self,o,v):
+        # print(domain+":")
+        # print(type)
+        # try:
+        #     eval("cl."+type[1:].upper()+"('"+domain+"')")
+        # except:
+        #     eval("cl."+type[2:].upper()+"('"+domain+"')")
+        # except:
+        #     eval("wh." + type[2:].upper() + "()")
+        # except:
+        #     eval("wh."+type[2:].upper()+"()")
 
+        if o in ("-a","-A"):
+            print(v)
+            cl.A(v)
+        elif o in ("-m","-M"):
+            print(v)
+            cl.M(v)
+        elif o in ("-n","-N"):
+            print(v)
+            cl.NS(v)
+        elif o in ("-c","-C"):
+            print(v)
+            cl.Cname(v)
+        elif o in ("-s","-S"):
+            print(v)
+            cl.SOA(v)
+        elif o in ("--all","--ALL"):
+            print(v)
+            cl.A(v)
+            cl.Cname(v)
+            cl.MX(v)
+            print(v)
+            cl.NS(v)
+            print(v)
+            cl.TXT(v)
+            cl.SOA(v)
+            print(v)
+            wh.Whois(v)
+        elif o in ("-w","-W"):
+            print(v)
+            wh.Whois(v)
+        elif o in ("--update","--UPDATE"):
+            print()
+            wh.Update()
+        else:
+            pass
+
+    def pro(self,o,v):
+        pass
 
 if __name__ == '__main__':
     dname =""
     resolver = resolver.Resolver()
     resolver.lifetime = 5
 
-    # filename = "domain.txt"
-    # with open(filename, "r", encoding="utf-8") as lines:
-    #     dname = lines.read()
 
     #读取命令行的选项
     option_arg = sys.argv[1:]
     try:
-        option,s = getopt.getopt(option_arg,"A:M:N:T:C:S:a:m:n:t:c:s:W:w:t:T:",["ALL=","all=","UPDATE=","update="])
-    except:
-        print("请输入合法参数!!")
-
-
+        option, s = getopt.getopt(option_arg, "a:m:n:s:t:w:", ["ALL=", "all=", "UPDATE=", "update=","proxy="])
+        print(option)
+    except getopt.GetoptError as e:
+        option,s = getopt.getopt(option_arg,"f:amnsw",["ALL","all","UPDATE","update","proxy="])
+    except BaseException as e:
+        print (e)
     #读取文件
 
     print (option)
+    #实例化对象
     cl = dnsinfo()
     wh = whoisinfo()
     ou = out()
-    # for o,v in option:
-    #     if o in ("-a","-A"):
-    #         print(v)
-    #         cl.A(v)
-    #     elif o in ("-m","-M"):
-    #         print(o)
-    #         cl.M(v)
-    #     elif o in ("-n","-N"):
-    #         print(o)
-    #         cl.NS(v)
-    #     elif o in ("-t"):
-    #         filename = v
-    #         with open(filename, "r", encoding="utf-8") as lines:
-    #             line = lines.read()
-    #             print(line)
-    #
-    #     elif o in ("-c","-C"):
-    #         print(o)
-    #         cl.Cname(v)
-    #     elif o in ("-s","-S"):
-    #         print(o)
-    #         cl.SOA(v)
-    #     elif o in ("--all","--ALL"):
-    #         print(v)
-    #         cl.A(v)
-    #         cl.Cname(v)
-    #         cl.MX(v)
-    #         print(v)
-    #         cl.NS(v)
-    #         print(v)
-    #         cl.TXT(v)
-    #         cl.SOA(v)
-    #     elif o in ("-w","-W"):
-    #         print(o)
-    #         wh.whois(v)
-    #     elif o in ("--update","--UPDATE"):
-    #         print(o)
-    #         wh.update()
-    #     else:
-    #         pass
-    #
-    for o,v in option:
-        for
-            ou.str(v,o)
+
+
+    #参数列表
+    list = []
+    for o, v in option:
+        if o in ("-f"):
+            filename = v
+            with open(filename, "r", encoding="utf-8") as lines:
+                line = str(lines.read())
+                line = line.strip()
+                list = line.split("\n")
+                option.pop(0)
+        else:
+            list = [v]
+
+    #结果输出
+    for o, v in option:
+        for i in list:
+                 ou.str(o,i)
--- try.py
+++ try.py
@@ -65,31 +65,119 @@ import dns
 # if __name__ == "__main__":
 #     main()
 #
-# import  requests
-#
-# reponse = requests.get("http://httpbin.org/ip")
-# print(reponse.text)
-# import requests
+import requests
 #
 # proxy = {
 #     'http': '218.244.147.59:3128'
 # }
 #
-# response = requests.get("http://httpbin.org/ip",proxies=proxy)
+# response = requests.get("http://账号:密码@proxycn2.huawei.com:8080",proxies=proxy)
 # print(response.text)
-import sys
-import getopt
+# import  requests
+#
+# reponse = requests.get("http://google.com")
+# print(reponse.text)
+#
+#
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'
+# }
+# proxies = {'http': 'http://127.0.0.1:1080' , 'https': 'http://127.0.0.1:1080'}
+# url = 'https://www.google.com'
+# res = requests.get(url, proxies=proxies, verify=False)
+# print(res.text)
 
-arg = sys.argv[1:]
-opthons,s= getopt.getopt(arg,"t:")
-print (opthons)
-for o,v in opthons:
-    if o in ("-t"):
-        filename = v
-        with open(filename, "r", encoding="utf-8") as lines:
-            line = lines.read()
-            print(line)
+# import sys
+# import getopt
+#
+# arg = sys.argv[1:]
+# try:
+#     opthons,s= getopt.getopt(arg,"at:")
+#     print (opthons)
+# except getopt.GetoptError as e :
+#     opthons, s = getopt.getopt(arg, "a:")
+#     print(opthons)
+# else:
+#     pass
+# for o,v in opthons:
+#     print(o)
+#     list = []
+#     if o in ("-t"):
+#         filename = v
+#
+#         with open(filename, "r", encoding="utf-8") as lines:
+#             line = str(lines.read())
+#             line = line.strip()
+#             list = line.split("\n")
+#         # print(list)
+#
+#     else:
+#         list=[v]
+#     print (list)
 
+#
+# a = [(1,2),(3,4),(5,6)]
+# a.pop(0)
+# print(a)
+import urllib3
+# from requests.packages.urllib3.contrib import pyopenssl as reqs
+#
+# ip='220.181.38.148'
+# port='443'
+# try:
+#     x509 = reqs.OpenSSL.crypto.load_certificate(
+#         reqs.OpenSSL.crypto.FILETYPE_PEM,
+#         reqs.ssl.get_server_certificate((ip, port))
+#     )
+#     domain = x509.get_subject().CN
+#     print(domain)
+# except:
+#     print(ip+'>Get CN failed')
+#
+# a = "123456789"
+#
+# print(a[-1][:-1])
 
+#!/usr/bin/python3
+#-*- coding:UTF-8 -*-
+import dns.resolver
+import os
+import httplib2
+iplist = []
+#appdomain = '.xn--zfr164b'
+appdomain = 'xn--zfr54hdx6e.xn--zfr164b'
+def get_iplist(domain = ''):
+  try:
+    A = dns.resolver.query(domain,'A')
+  except Exception as e:
+    print("dns resolver error %s" % str(e))
+    return
+  for i in A.response.answer:
+    for j in i.items:
+      iplist.append(j.address)
+  return True
 
-
+def checkip(ip):
+  #for ip in iplist:
+  checkurl =ip+":80"
+  getcontent = ""
+  httplib2.socket.setdefaulttimeout(5)
+  #conn = httplib2.HTTPConnection(checkurl) #httplib和httplib2的区别
+  conn = httplib2.Http()
+  try:
+    #conn.request("GET","/",header = {"Host":appdomain})
+    resp,getcontent = conn.request("http://"+checkurl)
+    #print("resp is %s" % resp)
+    #getcontent = resp.read(15)
+  finally:
+    #if getcontent == "<!doctype html>":#httplib2和httplib的区别
+    if resp['status'] == '200':
+      print("%s is OK!" % ip)
+    else:
+      print("%s is ERROR!" % ip)
+if __name__ == "__main__":
+  if get_iplist(appdomain) and len(iplist) >0 :
+    for ip in iplist:
+      checkip(ip)
+  else:
+    print("dns resolver error.")
